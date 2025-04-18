2dbf821b961e439b7260ff85a7cf61b1b77a5576
Max Niia <max.emil.niia@gmail.com>
2025-04-18 23:08:00 +0200

    feat: add crkbd keymap

---
+++ keyboards/crkbd/r2g/keymaps/max_niia/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE   = no     # Mouse keys
+RGBLIGHT_ENABLE   = no     # Enable WS2812 RGB underlight.
+OLED_ENABLE       = yes
+LTO_ENABLE        = yes
+CAPS_WORD_ENABLE  = yes
+RGB_MATRIX_ENABLE = yes
+#WPM_ENABLE = yes  # Enable word per minute counter
