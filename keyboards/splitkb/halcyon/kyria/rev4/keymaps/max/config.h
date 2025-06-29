173ae09ba50671edaca3f4a8277e5f5284c02545
Max Niia <max.emil.niia@gmail.com>
2025-06-29 11:02:35 +0200

    feat: add print screen, mouse keys and caps word

---
+++ keyboards/splitkb/halcyon/kyria/rev4/keymaps/max/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+
+#undef QUICK_TAP_TERM
+#define QUICK_TAP_TERM 110
+
+// #define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
--- keyboards/splitkb/halcyon/kyria/rev4/keymaps/max/keymap.c
+++ keyboards/splitkb/halcyon/kyria/rev4/keymaps/max/keymap.c
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT_split_3x6_5_hlc(
-     XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LCBR,
+     XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
      XXXXXXX,    GUIA,    ALTS,    CTLD,    SFTF,    HYPG,                                               HYPH,    SFTJ,    CTLK,    ALTL,    GUIC, KC_QUOT,
-     XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, MS_BTN1, MS_BTN2,        XXXXXXX, XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
-                                MS_BTN3,     MED,     NAV,    FUNC, XXXXXXX,        XXXXXXX,     SYM,     NUM,     ADJ,  KC_APP,
+     XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, MS_BTN1, MS_BTN2,        MS_BTN1, MS_BTN2,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+                                XXXXXXX,     MED,     NAV,    FUNC, MS_BTN3,        MS_BTN3,     SYM,     NUM,     ADJ,  KC_APP,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_MEDIA] = LAYOUT_split_3x6_5_hlc(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             XXXXXXX, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX,  GAMING,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             KC_PSCR, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX,  GAMING,
       XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_HYPR,                                             XXXXXXX, KC_MPLY, KC_MSTP, XXXXXXX, XXXXXXX,   RETRO,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
                                  XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, _______, _______, _______, XXXXXXX,
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAVIGATION] = LAYOUT_split_3x6_5_hlc(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_HYPR,                                             KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, CW_TOGG, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, XXXXXXX,
                                  XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,         XXXXXXX, _______, _______, _______, XXXXXXX,
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_FUNCTION] = LAYOUT_split_3x6_5_hlc(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             XXXXXXX,    KC_7,   KC_F8,   KC_F9,  KC_F12, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                             KC_PSCR,    KC_7,   KC_F8,   KC_F9,  KC_F12, XXXXXXX,
       XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_HYPR,                                             XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F10, XXXXXXX,
                                  XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,         XXXXXXX, _______, _______, _______, XXXXXXX,
@@ -153,9 +153,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_SYMBOLS] = LAYOUT_split_3x6_5_hlc(
-      XXXXXXX,   KC_LCBR,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                             XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_LCBR,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                            XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX,   KC_DQT,    KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                                             KC_HYPR,  LPAREN,  RPAREN, KC_LPRN, KC_LALT, KC_LGUI,
-      XXXXXXX,   KC_TILD,   KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_TILD,   KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX,
                                  XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, XXXXXXX,         XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
@@ -177,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT_split_3x6_5_hlc(
       XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                                             XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, KC_QUOT,    KC_4,    KC_5,    KC_6,  KC_EQL,                                             KC_HYPR,  LPAREN,  RPAREN, KC_LPRN, KC_LALT, KC_LGUI,
-      XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX,
                                  XXXXXXX,  KC_DOT,    KC_0, KC_MINS, XXXXXXX,         XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
@@ -293,5 +293,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     ),
 //
 };
+
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT_split_3x6_5_hlc(
+       'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+       'L', 'L', 'L', 'L', 'L', 'L',                       'R', 'R', 'R', 'R', 'R', 'R',
+       'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
+                      '*', '*', '*', '*', '*',   '*', '*', '*', '*', '*',
+       '*', '*', '*', '*', '*',                                 '*', '*', '*', '*', '*'
+    );
 
 // clang-format on
